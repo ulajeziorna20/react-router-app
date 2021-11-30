64f2cdf33f3544e1216d1049206576c7e1ba6de0
Ula Jeziorna <jeziornaula@wp.pl>
2021-11-30 21:50:11 +0100

    import komponentu Counter.js i UsersList.js, Próba dopasowania ścieżek path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css"
 import { Routes, Route } from "react-router-dom"
 // import Home from "./Home"
 // import About from "./About"
+import Counter from "./components/counter/Counter"
+import UsersList from "./components/userList/UsersList"
 
 function App() {
   return (
@@ -10,12 +12,12 @@ function App() {
       <header className="App-header">
         <h1>Welcome to React Router!</h1>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
+          <Route path="/" element={<Counter />} />
+          <Route path="users-list" element={<UsersList />} />
         </Routes>
       </header>
     </div>
-  );
+  )
 }
 
 export default App
