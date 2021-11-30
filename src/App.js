1c44c812e8c6f6da8b31e5edeff9fdf163c100e4
Ula Jeziorna <jeziornaula@wp.pl>
2021-11-30 21:42:29 +0100

    Stworzenie próbnych komponentów z react router. Home, About. Nauka jak to wszystko działa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react"
+import "./App.css"
+import { Routes, Route } from "react-router-dom"
+import Home from "./Home"
+import About from "./About"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to React Router!</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
