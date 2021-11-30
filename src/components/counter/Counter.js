22528e7920c5bf6a90d1c51bbb718db31ac040a1
Ula Jeziorna <jeziornaula@wp.pl>
2021-11-30 21:54:14 +0100

    formatowanie

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,72 +1,60 @@
-import React, { useState } from "react";
-import ButtonsPannel from "./ButtonsPannel";
-import ClockFunctional from "./ClockFunctional";
-import './css/Counter.css';
-import Display from "./Display";
-import Step from "./Step";
-import {  Link } from "react-router-dom";
-
+import React, { useState } from "react"
+import ButtonsPannel from "./ButtonsPannel"
+import ClockFunctional from "./ClockFunctional"
+import "./css/Counter.css"
+import Display from "./Display"
+import Step from "./Step"
+import { Link } from "react-router-dom"
 
 const Counter = (props) => {
+  const [counterValue, setCounterValue] = useState(props.initValue)
+  const [stepValue, setStepValue] = useState(1)
+  const [displayClock, setdisplayClock] = useState(true)
 
+  const showHideClock = (e) => {
+    setdisplayClock(!displayClock)
+  }
 
-    const [counterValue, setCounterValue] = useState(props.initValue);
-    const [stepValue, setStepValue] = useState(1);
-    const [displayClock, setdisplayClock] = useState(true);
+  const stepValueChange = (e) => {
+    setStepValue(+e.target.value)
+  }
 
+  const buttonHandleChange = (action) => {
+    let currentCounterValue = counterValue
+    let currentStepValue = stepValue
+    console.log(currentStepValue)
 
-    const showHideClock = (e) => {
-
-            setdisplayClock(!displayClock)
+    if (action === "add-x") {
+      currentCounterValue += currentStepValue
+    } else if (action === "reset") {
+      currentCounterValue = 0
+    } else {
+      currentCounterValue = props.initValue
     }
 
+    setCounterValue(currentCounterValue)
+  }
 
-
-
-    const stepValueChange = (e) => {
-
-        setStepValue(+e.target.value)
-    }
-
-
-    const buttonHandleChange = (action) => {
-
-
-        let currentCounterValue = counterValue
-        let currentStepValue = stepValue
-        console.log(currentStepValue);
-
-
-        if (action === 'add-x') {
-            currentCounterValue += currentStepValue
-
-        } else if (action === 'reset') {
-            currentCounterValue = 0
-        } else {
-            currentCounterValue = props.initValue
-
-        }
-
-        setCounterValue(currentCounterValue)
-    }
-
-    return (
-        <div>
-            <div>
-                <Display counterValueToDisplay={counterValue} />
-                <ButtonsPannel buttonMethod={buttonHandleChange}
-                    stepValueToDisplay={stepValue} />
-                <Step stepMethod={stepValueChange} />
-            </div>
-            <div>
-                {(displayClock ? <ClockFunctional clockMethod={showHideClock}/> : <span onClick={showHideClock}>Show clock</span>)}
-            </div>
-            <Link to="/users-list">About</Link>
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <Display counterValueToDisplay={counterValue} />
+        <ButtonsPannel
+          buttonMethod={buttonHandleChange}
+          stepValueToDisplay={stepValue}
+        />
+        <Step stepMethod={stepValueChange} />
+      </div>
+      <div>
+        {displayClock ? (
+          <ClockFunctional clockMethod={showHideClock} />
+        ) : (
+          <span onClick={showHideClock}>Show clock</span>
+        )}
+      </div>
+      <Link to="/users-list">About</Link>
+    </div>
+  )
 }
 
-
-
-
-export default Counter;
+export default Counter
--- src/components/userList/UsersList.js
+++ src/components/userList/UsersList.js
@@ -1,55 +1,55 @@
-import React, { useState } from 'react'
-import './css/UsersList.css'
-import ListToDisplay from './ListToDisplay'
-import {  Link } from "react-router-dom"
+import React, { useState } from "react"
+import "./css/UsersList.css"
+import ListToDisplay from "./ListToDisplay"
+import { Link } from "react-router-dom"
 
 const UsersList = () => {
-	const [name, setName] = useState('')
-	const [userList, setUserList] = useState([])
+  const [name, setName] = useState("")
+  const [userList, setUserList] = useState([])
 
-	const enterNewUser = (e) => {
-		setName(e.target.value)
-	}
+  const enterNewUser = (e) => {
+    setName(e.target.value)
+  }
 
-	const userListChange = () => {
-		setUserList((userList) => [...userList, { name: name, key: Date.now() }])
-		setName('')
-	}
+  const userListChange = () => {
+    setUserList((userList) => [...userList, { name: name, key: Date.now() }])
+    setName("")
+  }
 
-	const deleteUser = (id) => {
-		let filteredUsers = userList.filter((user) => {
-			if (user.key !== id) {
-				return true
-			}
-			return false
-		})
+  const deleteUser = (id) => {
+    let filteredUsers = userList.filter((user) => {
+      if (user.key !== id) {
+        return true
+      }
+      return false
+    })
 
-		setUserList(filteredUsers)
-	}
+    setUserList(filteredUsers)
+  }
 
-	return (
-		<section id="main-container">
-			<h1 id="main-header">User's List</h1>
-			<div>
-				<span id="input-text">
-					<input
-						type="text"
-						name="name"
-						id="enter-name"
-						placeholder="Enter name"
-						onChange={enterNewUser}
-						value={name}
-					/>
-				</span>
-				<button id="add-user-btn" onClick={userListChange}>
-					Add User
-				</button>
+  return (
+    <section id="main-container">
+      <h1 id="main-header">User's List</h1>
+      <div>
+        <span id="input-text">
+          <input
+            type="text"
+            name="name"
+            id="enter-name"
+            placeholder="Enter name"
+            onChange={enterNewUser}
+            value={name}
+          />
+        </span>
+        <button id="add-user-btn" onClick={userListChange}>
+          Add User
+        </button>
 
-				<ListToDisplay userList={userList} deleteUserMth={deleteUser} />
-			</div>
-			<Link to="/">Counter</Link>
-		</section>
-	)
+        <ListToDisplay userList={userList} deleteUserMth={deleteUser} />
+      </div>
+      <Link to="/">Counter</Link>
+    </section>
+  )
 }
 
 export default UsersList
